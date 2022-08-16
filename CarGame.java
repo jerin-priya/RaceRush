7b784224d6de0841246a64d0d0686d131d27fef7
Jerin Priya <74975012+jerin-priya@users.noreply.github.com>
2022-08-16 17:14:40 +0600

    continue

--- CarGame.java
+++ CarGame.java
@@ -175,4 +175,153 @@ class Racing{
         menu.add(help);
         frame.setJMenuBar(menu);
     }
+
+    public void createGame(){
+        LEFT_END=false;
+        RIGHT_END=false;
+        Thread thread = new Thread(new Runnable(){
+            public void run(){
+                runThread();
+            }
+        });
+        thread.setPriority(10);
+        thread.start();
+    }
+
+    public void runThread(){
+        try{
+            while(true){
+                label:
+                while(IS_GAME_OVER == false){
+                    int j=3,val3=0,val4=0;
+                    int POOL_YBOUND=0;
+                    cars[j] = new JButton(car1);
+                    cars[j+1]= new JButton(car1);
+                    cars[j+2]= new JButton(car1);
+                    cars[j+3]=new JButton(car1);
+                    cars[j].setBackground(Color.RED);
+                    cars[j+1].setBackground(Color.RED);
+                    cars[j+2].setBackground(Color.RED);
+                    cars[j+3].setBackground(Color.RED);
+                    cars[j].setBorder(null);
+                    cars[j+1].setBorder(null);
+                    cars[j+2].setBorder(null);
+                    cars[j+3].setBorder(null);
+                    int val1 = random.nextInt(3);
+                    int val2 = random.nextInt(3);
+                    if(val1==val2)
+                        val2=(val2+1)%3;
+                    if(VHARD==true){
+                        val3=(val1+1)%3;
+                        val4=(val2+1)%3;
+                    }
+                    for(int i=0;i<675;i++){
+                        if(NEWGAME==true){
+                            IS_GAME_OVER=true;
+                            break label;
+                        }
+                        while(PAUSE==true){
+                            Thread.sleep(1);
+                            if(NEWGAME==true){
+                                IS_GAME_OVER=true;
+                                break label;
+                            }
+                        }
+                        if(VHARD==true && k==0){
+                            cars[j+2].setBounds(75*(val3+1)+20,i,CAR_SIZEX-3,CAR_SIZEY);
+                            panel.add(cars[j+2]);
+                            if(i>=PCAR_YBOUND-60 && i<=(PCAR_YBOUND+60) && (75*(val3+1)+20) == PCAR_XBOUND){
+                                IS_GAME_OVER=true;
+                                break label;
+                            }
+                        }
+                        cars[j].setBounds(75*(val1+1)+20,i,CAR_SIZEX-3,CAR_SIZEY);
+                        if(i>225)
+                            cars[j+1].setBounds(75*(val2+1)+20,(i-225),CAR_SIZEX-3,CAR_SIZEY);
+                        if(i>=PCAR_YBOUND-60 && i<=(PCAR_YBOUND+60) && (75*(val1+1)+20) == PCAR_XBOUND) {
+                            IS_GAME_OVER=true;
+                            break label;
+                        }
+                        if(i>=(PCAR_YBOUND+225-60) && i<=(PCAR_YBOUND+285) && (75*(val2+1)+20)==PCAR_XBOUND){
+                            IS_GAME_OVER=true;
+                            break label;
+                        }
+                        if(score==1000 || score == 2000 || score == 3000 ){
+                            if(cars[0].getText().equalsIgnoreCase("Hard")){
+                                k=1;
+                                VHARD=true;
+                                GAME_LEVEL=1000;
+                                cars[0].setText("Very Hard");
+                                score+=50;
+                                continue;
+                            }
+                            else if(cars[0].getText().equalsIgnoreCase("Easy")){
+                                GAME_LEVEL=500;
+                                cars[0].setText("Normal");
+                                score+=50;
+                                continue;
+                            }
+                            else if(cars[0].getText().equalsIgnoreCase("Normal")){
+                                GAME_LEVEL=1000;
+                                cars[0].setText("Hard");
+                                score+=50;
+                                continue;
+                            }
+                            panel.add(cars[0]);
+                        }
+                        if(i%10==5){
+                            score+=1;
+                            Score.setText("Score:"+score);
+                            panel.add(Score);
+                        }
+                        if(i%330==329){
+                            CAR_COUNT++;
+                            Cars.setText("Cars:"+CAR_COUNT);
+                            panel.add(Cars);
+                        }
+                        panel.add(cars[j]);
+                        panel.add(cars[j+1]);
+                        Thread.sleep(1000/GAME_LEVEL);
+                    }
+                    panel.remove(cars[j]);
+                    panel.remove(cars[j+1]);
+                    if(VHARD==true){
+                        panel.remove(cars[j+2]);
+                        panel.remove(cars[j+3]);
+                    }
+                    k=0;
+                }
+                String comment;
+                if(score<1000) comment="Unlucky";
+                else if(score<2000) comment="Nice play";
+                else comment="Pretty Well Played";
+                if(NEWGAME!=true)JOptionPane.showMessageDialog(null,"\nGame over your score is "+score+" "+comment+" \nI think you should try again");
+                NEWGAME=false;
+                PAUSE=false;
+                cars[1].setFocusable(true);
+                cars[4].setBounds(75+20,450,CAR_SIZEX-3,CAR_SIZEY);
+                cars[3].setBounds(75+20,450,CAR_SIZEX-3,CAR_SIZEY);
+                panel.remove(cars[3]);
+                panel.remove(cars[4]);
+                if(VHARD==true){
+                    cars[5].setBounds(75+20,450,CAR_SIZEX-3,CAR_SIZEY);
+                    cars[6].setBounds(75+20,450,CAR_SIZEX-3,CAR_SIZEY);
+                    panel.remove(cars[6]);
+                    panel.remove(cars[5]);
+                }
+                IS_GAME_OVER=false;
+                score=0;
+                CAR_COUNT=0;
+                VHARD=false;
+                Score.setText("Score:"+score);
+                cars[0].setText("Easy");
+                Cars.setText("Cars:"+CAR_COUNT);
+                GAME_LEVEL=300;
+                continue;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
